de06cfe14de5eda8399affce92af47cae409309c
YukiMaitani <fightzoi1997@gmail.com>
2022-05-26 01:42:05 +0900

    Initial Commit

---
+++ Typical90/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Typical90
+//
+//  Created by 米谷裕輝 on 2022/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
